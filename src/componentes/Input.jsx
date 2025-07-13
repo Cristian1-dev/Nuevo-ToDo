178236a7864b1b5cf4227f38c66cde6bc4190101
Cristian1-dev <cristianros211@gmail.com>
2025-07-13 12:30:47 -0600

    Actualizando comentarios

--- src/componentes/Input.jsx
+++ src/componentes/Input.jsx
@@ -1,8 +1,16 @@
 import '../estilos/Input.css';
+// recibo texto, setTexto, y agregarTarea(funcion)
 export const Input = ({texto, setTexto, agregarTarea}) => {
     return (
+
         <div className="contenedor-input">
+            {/* Creo un input para ingresar una tarea  */}
+            {/* en el input establezco el valor con value={texto} */}
             <input className ='entradaTexto'type="text" value={texto} placeholder="Ingrese una tarea"
+                // onchange escucha un cambio y lo agrega a texto
+                // e.target.value es el valor que se ingresa en el input
+                // setTexto actualiza el valor de texto
+                // de e se extrae target y value
                 onChange={(e) => { setTexto(e.target.value);
                 }} />
                 
